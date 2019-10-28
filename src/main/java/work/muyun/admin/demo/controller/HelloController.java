ded10bde32c8133bc1182f2d3e09298372947778
Muyun99 <muyun@nuaa.edu.cn>
2019-10-28 21:56:03 +0800

    git workflow test2

--- src/main/java/work/muyun/admin/demo/controller/HelloController.java
+++ src/main/java/work/muyun/admin/demo/controller/HelloController.java
@@ -3,14 +3,15 @@ package work.muyun.admin.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-        123;
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/helloworld/{name}")
+    public String helloworld(@PathVariable("name") String name){
+        return "Hello " + name;
     }
 }
